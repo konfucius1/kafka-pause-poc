858b37d4494fc1fb60d9a08c706a8109320d0b08
Thanakon <kimsanthanakon@gmail.com>
2025-05-07 15:57:58 +1000

    feat: Initial commit

---
+++ src/test/kotlin/tkimsan/kafkapausepoc/KafkaPausePocApplicationTests.kt
@@ -0,0 +1,13 @@
+package tkimsan.kafkapausepoc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KafkaPausePocApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
