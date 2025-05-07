237a334e284034ca86050567f86143987eb30ef1
Thanakon <kimsanthanakon@gmail.com>
2025-05-07 23:41:42 +1000

    feat: Util kafka test topic creation config

---
+++ src/main/kotlin/tkimsan/kafkapausepoc/config/KafkaTopicConfig.kt
@@ -0,0 +1,20 @@
+package tkimsan.kafkapausepoc.config
+
+import org.apache.kafka.clients.admin.NewTopic
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.config.TopicBuilder
+
+@Configuration
+class KafkaTopicConfig(
+    @Value("\${poc.kafka.topic-name}") private val topicName: String
+) {
+
+    @Bean
+    fun pocTopic(): NewTopic {
+        return TopicBuilder.name(topicName)
+            .partitions(1)
+            .build()
+    }
+}
