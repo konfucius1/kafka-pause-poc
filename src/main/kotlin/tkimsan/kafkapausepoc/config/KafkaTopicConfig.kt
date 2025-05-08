027cffa022d4492b5910560a1e9d3bfa78e1b11d
Thanakon <kimsanthanakon@gmail.com>
2025-05-08 18:11:49 +1000

    feat: Create sink topic for testing

--- src/main/kotlin/tkimsan/kafkapausepoc/config/KafkaTopicConfig.kt
+++ src/main/kotlin/tkimsan/kafkapausepoc/config/KafkaTopicConfig.kt
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.admin.NewTopic
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
 import org.springframework.kafka.config.TopicBuilder
 
 @Configuration
@@ -14,6 +15,13 @@ class KafkaTopicConfig(
     @Bean
     fun pocTopic(): NewTopic {
         return TopicBuilder.name(topicName)
+            .partitions(1)
+            .build()
+    }
+
+    @Bean
+    fun sinkTopic(): NewTopic {
+        return TopicBuilder.name("flaky-output-topic")
             .partitions(1)
             .build()
     }
